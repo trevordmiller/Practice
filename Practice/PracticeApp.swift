05d6d821c1ce8e289fbaa9c7d89b0e74dfa8cbda
Trevor Miller <5497885+trevordmiller@users.noreply.github.com>
2022-11-23 13:10:55 -0700

    Add generated files from new Xcode project

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Trevor Miller on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
